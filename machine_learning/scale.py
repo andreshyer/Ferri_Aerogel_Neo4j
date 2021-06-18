5ca0699b5c0bde54f6cb4313bfe640980cbc2d22
qle <quangle270799@gmail.com>
2021-06-18 09:54:45 -0400

    fix docstring params

--- machine_learning/scale.py
+++ machine_learning/scale.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import numpy as np
 
 
 class Scaler:
@@ -6,9 +7,9 @@ class Scaler:
     def scale_data(scale, train_features, test_features, val_features=None):
         """
         Description: Scale train, test, split data according to the :param scale.
-        :param x_test: test feature
-        :param x_train: train feature
-        :param x_val: val feature
+        :param test_features: test feature
+        :param train_features: train feature
+        :param val_features: val feature
         :param scale: scale method.
             std is for StandardScaler, minmax is for MinMaxScaler, None will skip
         :return:
@@ -23,7 +24,11 @@ class Scaler:
         if scale is not None:
             train_features = scale.fit_transform(train_features)
             test_features = scale.transform(test_features)
+
+#            test_features, train_features= np.array(test_features), np.array(train_features)
             if val_features is not None:
                 val_features = scale.transform(val_features)
+                
+#                val_features = np.array(val_features)
                 return test_features, train_features, val_features
         return test_features, train_features
